e6a477147daf9b43a88273bb56a389dc37b5e386
pbullhove <pbullhove>
2024-09-27 08:47:47 +0000

    mae bronze get data from multiple localities and depths

--- databricks/Havvarsel - Transform to Gold.py
+++ databricks/Havvarsel - Transform to Gold.py
@@ -10,10 +10,10 @@ save_df_as_csv(df_silver, "/gold/hav_temperature_projection_latest")
 
 # COMMAND ----------
 
-from helpers.adls_utils import get_adls_file_path
+from helpers.adls_utils import get_adls_folder_path
 from datetime import datetime
 
-gold_path = f"{get_adls_file_path()}/gold"
+gold_path = f"{get_adls_folder_path()}/gold"
 hav_gold_folder= f"{gold_path}/hav_temperature_projection_latest"
 files = dbutils.fs.ls(hav_gold_folder)
 
