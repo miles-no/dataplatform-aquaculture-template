cd2d35ef00d8233ca51d6a27744c1f0b91be6955
Peter Bull Hove <peterbullhove@gmail.com>
2024-10-03 11:26:27 +0200

    feat(api): make swagger available in prod as well

--- api/AquaApi/Program.cs
+++ api/AquaApi/Program.cs
@@ -20,37 +20,39 @@ builder.Services.AddSingleton(x =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "AquaApi is running. Navigate to swagger to test.");
 
 // Endpoint to fetch a CSV from Blob Storage, parse it, and return JSON
-app.MapGet("/temperature_predictions/latest", async (BlobServiceClient blobServiceClient) =>
-    {
-        var containerClient = blobServiceClient.GetBlobContainerClient("datalake");
-        var blobClient = containerClient.GetBlobClient("havvarsel/gold/havtemp-pred-latest.csv");
-
-        // Check if the file exists
-        if (!await blobClient.ExistsAsync())
+app.MapGet(
+        "/temperature_predictions/latest",
+        async (BlobServiceClient blobServiceClient) =>
         {
-            return Results.NotFound(new { message = "File not found" });
+            var containerClient = blobServiceClient.GetBlobContainerClient("datalake");
+            var blobClient = containerClient.GetBlobClient(
+                "havvarsel/gold/havtemp-pred-latest.csv"
+            );
+
+            // Check if the file exists
+            if (!await blobClient.ExistsAsync())
+            {
+                return Results.NotFound(new { message = "File not found" });
+            }
+
+            // Download the CSV file from Blob Storage
+            var downloadStream = new MemoryStream();
+            await blobClient.DownloadToAsync(downloadStream);
+            downloadStream.Position = 0; // Reset the stream position
+
+            // Parse the CSV file using CsvHelper
+            using var reader = new StreamReader(downloadStream);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<MeasurementRow>().ToList(); // Dynamically read CSV data
+            return Results.Ok(records); // Return the JSON data
         }
-
-        // Download the CSV file from Blob Storage
-        var downloadStream = new MemoryStream();
-        await blobClient.DownloadToAsync(downloadStream);
-        downloadStream.Position = 0; // Reset the stream position
-
-        // Parse the CSV file using CsvHelper
-        using var reader = new StreamReader(downloadStream);
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        var records = csv.GetRecords<MeasurementRow>().ToList(); // Dynamically read CSV data
-        return Results.Ok(records); // Return the JSON data
-    })
+    )
     .WithName("DownloadCsvAndParseToJson");
 
 app.Run("http://0.0.0.0:80");
