b86a192c35d5ae2b575e42056bfc49012a890ae0
Peter Bull Hove <peterbullhove@gmail.com>
2024-10-09 15:42:20 +0200

    ci(function): make funtion configuration correct

--- terraform/application_insights.tf
+++ terraform/application_insights.tf
@@ -3,4 +3,14 @@ resource "azurerm_application_insights" "apin" {
   location            = azurerm_resource_group.data_platform.location
   resource_group_name = azurerm_resource_group.data_platform.name
   application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.log.id
+
+}
+
+resource "azurerm_log_analytics_workspace" "log" {
+  name                = "workspace-test"
+  location            =  azurerm_resource_group.data_platform.location
+  resource_group_name =  azurerm_resource_group.data_platform.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
